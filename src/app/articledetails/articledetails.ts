5ee8c0e7ec942edae04e9119d2ecf65521ab5ee6
Deepak-Singh <deepak.singh@coxautoinc.com>
2025-11-20 15:13:12 +0530

    added comment section

--- src/app/articledetails/articledetails.ts
+++ src/app/articledetails/articledetails.ts
@@ -29,6 +29,43 @@ export class ArticleDetailsComponent {
     { title: 'Understanding Smart Contracts' },
     { title: 'How NFTs are Changing the Art World' }
   ];
+
+  showComments = true;
+
+  comments = [
+    {
+      id: 1,
+      author: 'Anna',
+      time: '12:03 PM',
+      text: 'Ut in ad laborum minim aliqua mollit proident enim adipiscing...',
+      replies: []
+    },
+    {
+      id: 2,
+      author: 'John',
+      time: '08:10 AM',
+      text: 'Id ullamco qui tempor consectetur fugiat magna officia eiusmod...',
+      replies: [
+        {
+          author: 'Benjamin Foster',
+          time: '08:20 AM',
+          role: 'Author',
+          text: 'Ipsum anim est consequat commodo reprehenderit mollit enim...',
+        }
+      ]
+    },
+    {
+      id: 3,
+      author: 'Lisa',
+      time: '10:50 AM',
+      text: 'Anim aute ea ad dolore et enim aute duis laboris officia...',
+      replies: []
+    }
+  ];
+
+  toggleComments() {
+    this.showComments = !this.showComments;
+  }
 }
 
 
